34d9b5bd1cf5f19f2820388ad545897e42a65924
Celso Tavares <antoniocelsojr@hotmail.com>
2022-11-29 20:28:10 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -61,9 +61,9 @@ function submit(){
         
     }else {
       alert('Parabéns, todos os dados foram cadastrados.')
-      name = ''
-      email = ''
-      password1 = ''
+       setName('')
+       setEmail('')
+       setPassword('')
     }
  }
 
